71def66278d4c4809e0f6eeb6a8dce36a5405a67
AdrianDolniak <AdrianDolniak@users.noreply.github.com>
2019-07-09 21:10:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Program
+{
+    public static class Math
+    {
+        public static double F1(double x, double y = 0) => System.Math.Pow(x, 2) + y;
+
+        /*public static string F2(string x)
+        {
+            if int x == [];
+            
+        }*/
+    }
+}
---
+++ tc_Program.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Program;
+
+namespace Tests
+{
+    public class TestSuite1
+    {
+        [Test]
+        public void F01_1()
+        {
+            Assert.AreEqual(0, Math.F1(x: 0));
+        }
+
+        [Test]
+        public void F01_2()
+        {
+            Assert.AreEqual(1, Math.F1(x: 1));
+        }
+
+        [Test]
+        public void F01_3()
+        {
+            Assert.AreEqual(4, Math.F1(x: 2));
+        }
+
+        [Test]
+        public void F01_4()
+        {
+            Assert.AreEqual(5, Math.F1(x: 2, y: 1));
+        }
+
+        [Test]
+        public void F01_5()
+        {
+            Assert.AreEqual(7, Math.F1(x: 2, y: 3));
+        }
+
+        [Test]
+        public void F02_1()
+        {
+            Assert.True(true);
+        }
+    }
+}
