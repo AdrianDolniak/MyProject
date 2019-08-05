eb282907e9bb33ef228543e7d5587dd496d2a530
Adrian Dolniak <adolniak@objectivity.co.uk>
2019-08-05 15:16:46 +0200

    new file

---
+++ program/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ProjectCSharp.program
+{
+    public static class Calculator
+    {
+        public static float Multiply(float arg1, float arg2)
+        {
+            return (arg1 * arg2);
+        }
+
+        public static float Divide(float arg1, float arg2)
+        {
+            return (arg1 / arg2);
+        }
+
+        public static float Adding(float arg1, float arg2)
+        {
+            return (arg1 + arg2);
+        }
+
+        public static float Subtract(float arg1, float arg2)
+        {
+            return (arg1 - arg2);
+        }
+
+        public static float Modulo(float arg1, float arg2)
+        {
+            return (arg1 % arg2);
+        }
+
+    }
+
+}
---
+++ tests/tc_calculator.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using ProjectCSharp.program;
+
+namespace ProjectCSharp.tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void TestCalculatorAdding()
+        {
+            Assert.AreEqual(30, Calculator.Adding(10, 20));         
+        }
+        [Test]
+        public void TestCalculatorSubtract()
+        {
+            Assert.AreEqual(5, Calculator.Subtract(15, 10));
+        }
+        [Test]
+        public void TestCalculatorMultiply()
+        {
+            Assert.AreEqual(35, Calculator.Multiply(5, 7));         
+        }
+        [Test]
+        public void TestCalculatorDivide()
+        {
+            Assert.AreEqual(4, Calculator.Divide(24, 6));        
+        }
+        [Test]
+        public void TestCalculatorModulo()
+        {
+            Assert.AreEqual(0, Calculator.Modulo(6, 6));
+            Assert.AreEqual(0, Calculator.Modulo(8, 4));
+            Assert.AreEqual(0, Calculator.Modulo(100, 10));
+        }
+    }
+}
